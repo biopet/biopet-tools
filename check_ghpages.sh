54774a203cd7a39e72f4079bca8541cfd1e4ad0a
pjvan_thof <pjrvanthof@gmail.com>
2018-01-09 12:03:52 +0100

    Fixing grep

--- check_ghpages.sh
+++ check_ghpages.sh
@@ -2,7 +2,7 @@
 
 for I in `echo */build.sbt | sed "s/\/build.sbt//g"`
 do 
-	echo "$I "`curl -I https://biopet.github.io/$I/develop/index.html | grep HTTP/2`
+	echo "$I "`curl -I https://biopet.github.io/$I/develop/index.html | grep HTTP`
 done
 
 
